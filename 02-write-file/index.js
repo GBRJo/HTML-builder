fdcb22ce2db787e84bc53cb3350876a9e20e3178
GBRJo <anchernyakova@yandex.ru>
2024-01-20 00:49:32 +0400

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, '02-write-file.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question('(^･ｪ･^) Are you a cat?: ', function (input) {
+  fs.writeFile(filePath, input + '\n', function (error) {
+    if (error) {
+      console.log('(=^‥^)/ Somesign went wrong: ' + error);
+    } else {
+      console.log('(^-ｪ-^) I have saved your response');
+    }
+    secondQuestion();
+    process.on('exit', function () {
+      console.log('\nฅ(^･ｪ･^) Bye! It is better for you to be a cat.');
+    });
+  });
+});
+
+function secondQuestion() {
+  rl.question('(^･ｪ･^) Are you shure?: ', function (input) {
+    fs.appendFile(filePath, input + '\n', function (error) {
+      if (error) {
+        console.log('(=^‥^)/ Somesign went wrong: ' + error);
+      } else {
+        console.log('(^-ｪ-^) I have saved your response');
+      }
+      secondQuestion();
+    });
+  });
+}
