c07aaf8ef99a7f77275098736a34cb74ef201f43
GBRJo <anchernyakova@yandex.ru>
2024-01-21 04:24:00 +0400

    fix: 03-files-in-folder delete sync function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,21 +2,27 @@ const path = require('path');
 const fs = require('fs');
 
 const filePath = path.join(__dirname, 'secret-folder');
-const files = fs.readdirSync(filePath, { withFileTypes: true });
-for (let i = 0; i < files.length; i++) {
-  if (files[i].isFile()) {
-    const fulName = files[i].name;
-    const ext = path.extname(fulName).slice(1);
-    const name = fulName.slice(0, fulName.lastIndexOf('.'));
-    const fileWay = path.join(__dirname, 'secret-folder', fulName);
-    fs.stat(fileWay, function (err, stats) {
-      if (err) {
-        console.log('Something went wrong: ' + err);
-      } else {
-        const size = stats.size;
-        const sizeKbytes = size / 1024;
-        console.log(name + ' - ' + ext + ' - ' + sizeKbytes + 'kb');
+
+fs.readdir(filePath, { withFileTypes: true }, function (error, files) {
+  if (error) {
+    console.log('Something went wrong: ' + error);
+  } else {
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].isFile()) {
+        const fullName = files[i].name;
+        const ext = path.extname(fullName).slice(1);
+        const name = fullName.slice(0, fullName.lastIndexOf('.'));
+        const fileWay = path.join(filePath, fullName);
+        fs.stat(fileWay, function (err, stats) {
+          if (err) {
+            console.log('Something went wrong: ' + err);
+          } else {
+            const size = stats.size;
+            const sizeKbytes = size / 1024;
+            console.log(name + ' - ' + ext + ' - ' + sizeKbytes + 'kb');
+          }
+        });
       }
-    });
+    }
   }
-}
+});
