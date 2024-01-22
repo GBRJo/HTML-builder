01a77ef699746dde13a834a500ab488f2c6f2f09
GBRJo <anchernyakova@yandex.ru>
2024-01-22 05:33:38 +0400

    feat: add 06-build-page full solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const templatePath = path.join(__dirname, 'template.html');
 const componentsPath = path.join(__dirname, 'components');
-const stylesPath = path.join(__dirname, 'styles');
-const assetsPath = path.join(__dirname, 'assets');
+const pathStyles = path.join(__dirname, 'styles');
+const pathProjectDist = path.join(__dirname, 'project-dist', 'style.css');
 const distPath = path.join(__dirname, 'project-dist');
+const assetsPath = path.join(distPath, 'assets');
 const indexPath = path.join(distPath, 'index.html');
+const readFolder = path.join(__dirname, 'assets');
 
 // Создаем папку project-dist
 fs.mkdir(distPath, { recursive: true })
@@ -49,6 +51,85 @@ fs.mkdir(distPath, { recursive: true })
                   });
               }
             }
+          })
+          .catch((error) => {
+            console.log('Something went wrong: ' + error);
+          });
+      })
+      .catch((error) => {
+        console.log('Something went wrong: ' + error);
+      });
+    // Компилируем стили из stylesпапки в один файл и помещаем его в project-dist/style.css.
+    fs.readdir(pathStyles, { withFileTypes: true })
+      .then((files) => {
+        for (let i = 0; i < files.length; i++) {
+          if (files[i].isFile()) {
+            const fileName = files[i].name;
+            const ext = path.extname(fileName);
+            const filePath = path.join(pathStyles, fileName);
+            if (ext === '.css') {
+              fs.readFile(filePath, 'utf8')
+                .then((data) => {
+                  const cssContent = data;
+                  fs.appendFile(pathProjectDist, cssContent)
+                    .then(() => {
+                      console.log('Successfully wrote to ' + pathProjectDist);
+                    })
+                    .catch((error) => {
+                      console.log('Something went wrong: ' + error);
+                    });
+                })
+                .catch((error) => {
+                  console.log('Something went wrong: ' + error);
+                });
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        console.log('Something went wrong: ' + error);
+      });
+    // Копируем assets папку в project-dist/assets
+    fs.mkdir(assetsPath, { recursive: true })
+      .then(() => {
+        console.log('Folder created successfully at: ' + assetsPath);
+
+        fs.readdir(readFolder)
+          .then((files) => {
+            console.log('Folder read successfully at: ' + readFolder);
+
+            files
+              .forEach((file) => {
+                const filePath = path.join(readFolder, file);
+                const assetsFilePath = path.join(assetsPath, file);
+                fs.mkdir(assetsFilePath, { recursive: true }).then(() => {
+                  console.log(
+                    'Folder created successfully at: ' + assetsFilePath,
+                  );
+
+                  fs.readdir(filePath, 'utf8')
+                    .then((subFiles) => {
+                      subFiles.forEach((subFile) => {
+                        const fileFrom = path.join(filePath, subFile);
+                        const fileTo = path.join(assetsFilePath, subFile);
+
+                        fs.copyFile(fileFrom, fileTo)
+                          .then(() => {
+                            console.log('File copied successfully: ' + subFile);
+                          })
+                          .catch((error) => {
+                            console.log('Something went wrong: ' + error);
+                          });
+                      });
+                    })
+                    .catch((error) => {
+                      console.log('Something went wrong: ' + error);
+                    });
+                });
+              })
+              .catch((error) => {
+                console.log('Something went wrong: ' + error);
+              });
           })
           .catch((error) => {
             console.log('Something went wrong: ' + error);
