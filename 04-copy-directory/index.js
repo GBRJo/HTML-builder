a60ad214d7c5c29c4b4b7db5dc002dec3ab06665
GBRJo <anchernyakova@yandex.ru>
2024-01-25 23:22:57 +0400

    fix:04-copy-directory: now function delete not actual files from files-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,20 +7,48 @@ fs.mkdir(joinPath, { recursive: true })
     console.log('Folder created successfully at: ' + joinPath);
 
     const readFolder = path.join(__dirname, 'files');
-    fs.readdir(readFolder)
-      .then((files) => {
-        console.log('Folder read successfully at: ' + readFolder);
-        files.forEach((file) => {
-          const fileFrom = path.join(readFolder, file);
-          const fileTo = path.join(joinPath, file);
-          fs.copyFile(fileFrom, fileTo)
-            .then(() => {
-              console.log('File copied successfully: ' + file);
-            })
-            .catch((error) => {
-              console.log('Something went wrong: ' + error);
+    fs.readdir(joinPath)
+      .then((copyFiles) => {
+        // Получаем список файлов в папке files-copy
+        fs.readdir(readFolder)
+          .then((sourceFiles) => {
+            console.log('Folder read successfully at: ' + readFolder);
+            // Удаляем файлы, которые не существуют в папке files
+            copyFiles.forEach((copyFile) => {
+              if (!sourceFiles.includes(copyFile)) {
+                const fileToDelete = path.join(joinPath, copyFile);
+                fs.unlink(fileToDelete)
+                  .then(() => {
+                    console.log('File deleted successfully: ' + copyFile);
+                  })
+                  .catch((error) => {
+                    console.log('Something went wrong: ' + error);
+                  });
+              }
             });
-        });
+
+            fs.readdir(readFolder)
+              .then((files) => {
+                console.log('Folder read successfully at: ' + readFolder);
+                files.forEach((file) => {
+                  const fileFrom = path.join(readFolder, file);
+                  const fileTo = path.join(joinPath, file);
+                  fs.copyFile(fileFrom, fileTo)
+                    .then(() => {
+                      console.log('File copied successfully: ' + file);
+                    })
+                    .catch((error) => {
+                      console.log('Something went wrong: ' + error);
+                    });
+                });
+              })
+              .catch((error) => {
+                console.log('Something went wrong: ' + error);
+              });
+          })
+          .catch((error) => {
+            console.log('Something went wrong: ' + error);
+          });
       })
       .catch((error) => {
         console.log('Something went wrong: ' + error);
